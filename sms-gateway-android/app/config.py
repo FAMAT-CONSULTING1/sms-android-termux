533d57a8c4049074980a97f2b1178abf19bd50d6
MarxAlonso <marxchip99@gmail.com>
2026-05-20 09:31:35 -0500

    feat: agregando nueva url

--- sms-gateway-android/app/config.py
+++ sms-gateway-android/app/config.py
@@ -4,7 +4,7 @@ import pydantic
 class AppSettings(pydantic.BaseSettings):
     redis_url = pydantic.RedisDsn(url="redis://localhost/", scheme="redis")
     redis_port: int = 6379
-    backend_url: str = "https://backend-production-8462.up.railway.app"
+    backend_url: str = "https://api.cobralo.pe"
     admin_email: str = "bensamuel114@gmail.com"
     admin_password: str = "admin12345"
     pending_sms_path: str = "/calendario/delivery-jobs/sms-pendientes"
